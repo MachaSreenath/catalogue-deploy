93c2471811d5f5e12f4ed6d44718cd77d705d963
MachaSreenath <sreenathshree9@gmail.com>
2025-02-20 17:06:48 +0530

    catalogue-deploy changes

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 component=$1
-environment=$2 # don't use env   
+environment=$2 # don't use env
+app_version=$3
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
-ansible-pull -U https://github.com/MachaSreenath/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment main-tf.yaml
+ansible-pull -U https://github.com/MachaSreenath/roboshop-ansible-roles-tf.git -e component=$component -e env=$environment -e app_version=$app_version main-tf.yaml
 
